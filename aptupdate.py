2e9c6c879d9205664df4d1a5589192b751d34737
root <root@merluza.cloudedge.ru>
2013-06-07 02:09:05 +0400

    autoupdate fix

--- aptupdate.py
+++ aptupdate.py
@@ -11,11 +11,13 @@ osutils.beroot()
 
 if openstack_conf.version in ["folsom", "grizzly"]:
 
-  packages_str = 'apt-get -y install ubuntu-cloud-keyring python-software-properties software-properties-common python-keyring'
-  sources_list = 'echo deb http://ubuntu-cloud.archive.canonical.com/ubuntu precise-updates/' + openstack_conf.version + ' main >> /etc/apt/sources.list.d/openstack.list'
+  if not os.path.exists('/etc/apt/sources.list.d/openstack.list'):
 
-  osutils.run_std(packages_str)
-  osutils.run_std(sources_list)
+    packages_str = 'apt-get -y install ubuntu-cloud-keyring python-software-properties software-properties-common python-keyring'
+    sources_list = 'echo deb http://ubuntu-cloud.archive.canonical.com/ubuntu precise-updates/' + openstack_conf.version + ' main >> /etc/apt/sources.list.d/openstack.list'
+
+    osutils.run_std(packages_str)
+    osutils.run_std(sources_list)
 
 osutils.run_std('apt-get update')
 osutils.run_std('apt-get -y upgrade')
